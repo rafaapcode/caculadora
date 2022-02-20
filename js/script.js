e318abd21246a8b52327b8a26e2d34251274b788
rafaapcode <rafaap2003@gmail.com>
2022-02-20 08:27:54 -0300

    js - initial

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-const btns = [$(".btn0"), $(".btn1"), $(".btn2"), $(".btn3"), $(".btn4"), $(".btn5"), $(".btn6"), $(".btn7"), $(".btn8"), $(".btn9"), $(".btn10"), $(".btn11"), $(".btn12"), $(".btn13"), $(".btn14"), $(".btn15"), $(".btn16"), $(".btn17"), $(".btn18"), $(".btn19")];
-
-for(let i = 0; i < btns.length; i++){
-
-    
+const res = $("[key='resposta']");
 
 
-}
+function insert(n){
+    if(n === 'CE' || n === 'C'){
+        res.text("");
+    }else{
+        let num = res.text();
+        res.text(num + n);
+    }
+}   
